deba66b610885f894a422f6cc2fe054756573eb8
emanyousof <emanyousof12@gmail.com>
2025-03-28 09:32:33 +0200

    Add files via upload

---
+++ sss.js
@@ -0,0 +1,10 @@
+const booton=document.getElementById("booton")
+const btn=document.createElement("button")
+
+
+const conter=function(){
+    booton.prepend("btn")
+btn.classList.add("rad")
+btn.innerText="AEED SAED MAGDY"
+}
+setTimeout(conter, 3000)
